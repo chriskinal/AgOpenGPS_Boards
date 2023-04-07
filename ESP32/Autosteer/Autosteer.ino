7071d07e235919d5a63a252f246e527877a8b162
MrPoke21 <mrpoke21@gmail.com>
2023-04-07 21:50:54 +0200

    Minden kezdet nehéz...

---
+++ ESP32/Autosteer/Autosteer.ino
@@ -0,0 +1,289 @@
+#include <TaskScheduler.h>
+#include "SparkFun_BNO080_Arduino_Library.h"  // Click here to get the library: http://librarymanager/All#SparkFun_BNO080
+BNO080 myIMU;
+
+/*  PWM Frequency ->
+     490hz (default) = 0
+     122hz = 1
+     3921hz = 2
+*/
+#define PWM_Frequency 0
+
+/////////////////////////////////////////////
+
+// if not in eeprom, overwrite
+#define EEP_Ident 2400
+
+//   ***********  Motor drive connections  **************888
+//Connect ground only for cytron, Connect Ground and +5v for IBT2
+
+//Dir1 for Cytron Dir, Both L and R enable for IBT2
+#define DIR1_RL_ENABLE 4
+
+//PWM1 for Cytron PWM, Left PWM for IBT2
+#define PWM1_LPWM 2
+
+//Not Connected for Cytron, Right PWM for IBT2
+#define PWM2_RPWM 3
+
+//--------------------------- Switch Input Pins ------------------------
+#define STEERSW_PIN 32
+#define WORKSW_PIN 34
+#define REMOTE_PIN 37
+
+//Define sensor pin for current or pressure sensor
+#define CURRENT_SENSOR_PIN A17
+#define PRESSURE_SENSOR_PIN A10
+
+#define CONST_180_DIVIDED_BY_PI 57.2957795130823
+
+//How many degrees before decreasing Max PWM
+#define LOW_HIGH_DEGREES 3.0
+
+#include <WiFiManager.h>
+#include "AsyncUDP.h"
+#include <elapsedMillis.h>
+#include <EEPROM.h>
+#include <Wire.h>
+#include "zNMEAParser.h"
+
+AsyncUDP udp;
+
+IPAddress myip;
+
+Scheduler ts;
+
+void readBNO();
+
+Task t1(TASK_IMMEDIATE, TASK_FOREVER, &readBNO, &ts, true);
+
+//loop time variables in microseconds
+const uint16_t LOOP_TIME = 25;  //40Hz
+uint32_t autsteerLastTime = LOOP_TIME;
+uint32_t currentTime = LOOP_TIME;
+
+const uint16_t WATCHDOG_THRESHOLD = 100;
+const uint16_t WATCHDOG_FORCE_VALUE = WATCHDOG_THRESHOLD + 2;  // Should be greater than WATCHDOG_THRESHOLD
+uint8_t watchdogTimer = WATCHDOG_FORCE_VALUE;
+
+uint8_t aog2Count = 0;
+float sensorReading;
+float sensorSample;
+
+elapsedMillis gpsSpeedUpdateTimer = 0;
+
+//EEPROM
+int16_t EEread = 0;
+
+//Relays
+bool isRelayActiveHigh = true;
+uint8_t relay = 0, relayHi = 0, uTurn = 0;
+uint8_t tram = 0;
+
+//Switches
+uint8_t remoteSwitch = 0, workSwitch = 0, steerSwitch = 1, switchByte = 0;
+
+//On Off
+uint8_t guidanceStatus = 0;
+uint8_t prevGuidanceStatus = 0;
+bool guidanceStatusChanged = false;
+
+//speed sent as *10
+float gpsSpeed = 0;
+bool GGA_Available = false;    //Do we have GGA on correct port?
+
+// booleans to see if we are using BNO08x
+bool useBNO08x = false;
+
+float roll = 0;
+float pitch = 0;
+float yaw = 0;
+
+/* A parser is declared with 3 handlers at most */
+NMEAParser<2> parser;
+
+const bool invertRoll= true;  //Used for IMU with dual antenna
+
+//Fusing BNO with Dual
+double rollDelta;
+double rollDeltaSmooth;
+double correctionHeading;
+double gyroDelta;
+double imuGPS_Offset;
+double gpsHeading;
+double imuCorrected;
+#define twoPI 6.28318530717958647692
+#define PIBy2 1.57079632679489661923
+
+#define RAD_TO_DEG_X_10 572.95779513082320876798154814105
+
+#define REPORT_INTERVAL 20  //BNO report time, we want to keep reading it quick & offen. Its not timmed to anything just give constant data.
+
+//steering variables
+float steerAngleActual = 0;
+float steerAngleSetPoint = 0;  //the desired angle from AgOpen
+int16_t steeringPosition = 0;  //from steering sensor
+float steerAngleError = 0;     //setpoint - actual
+
+//pwm variables
+int16_t pwmDrive = 0, pwmDisplay = 0;
+float pValue = 0;
+float errorAbs = 0;
+float highLowPerDeg = 0;
+
+//Steer switch button  ***********************************************************************************************************
+uint8_t currentState = 1, reading, previous = 0;
+uint8_t pulseCount = 0;  // Steering Wheel Encoder
+bool encEnable = false;  //debounce flag
+uint8_t thisEnc = 0, lastEnc = 0;
+
+unsigned int portMy = 5120;  // port of this module
+
+//Variables for settings
+struct Storage {
+  uint8_t Kp = 40;      // proportional gain
+  uint8_t lowPWM = 10;  // band of no action
+  int16_t wasOffset = 0;
+  uint8_t minPWM = 9;
+  uint8_t highPWM = 60;  // max PWM value
+  float steerSensorCounts = 30;
+  float AckermanFix = 1;  // sent as percent
+};
+Storage steerSettings;  // 11 bytes
+
+//Variables for settings - 0 is false
+struct Setup {
+  uint8_t InvertWAS = 0;
+  uint8_t IsRelayActiveHigh = 0;  // if zero, active low (default)
+  uint8_t MotorDriveDirection = 0;
+  uint8_t SingleInputWAS = 1;
+  uint8_t CytronDriver = 1;
+  uint8_t SteerSwitch = 0;  // 1 if switch selected
+  uint8_t SteerButton = 0;  // 1 if button selected
+  uint8_t ShaftEncoder = 0;
+  uint8_t PressureSensor = 0;
+  uint8_t CurrentSensor = 0;
+  uint8_t PulseCountMax = 5;
+  uint8_t IsDanfoss = 0;
+  uint8_t IsUseY_Axis = 0;  //Set to 0 to use X Axis, 1 to use Y avis
+};
+Setup steerConfig;  // 9 bytes
+
+bool Autosteer_running = true;  //Auto set off in autosteer setup
+
+void steerSettingsInit() {
+  // for PWM High to Low interpolator
+  highLowPerDeg = ((float)(steerSettings.highPWM - steerSettings.lowPWM)) / LOW_HIGH_DEGREES;
+}
+
+void autosteerSetup() {
+  //PWM rate settings. Set them both the same!!!!
+  /*  PWM Frequency ->
+       490hz (default) = 0
+       122hz = 1
+       3921hz = 2
+  */
+  if (PWM_Frequency == 0) {
+    ledcSetup(PWM1_LPWM, 490, 8);
+    ledcSetup(PWM2_RPWM, 490, 8);
+  } else if (PWM_Frequency == 1) {
+    ledcSetup(PWM1_LPWM, 122, 8);
+    ledcSetup(PWM2_RPWM, 122, 8);
+  } else if (PWM_Frequency == 2) {
+    ledcSetup(PWM1_LPWM, 3921, 8);
+    ledcSetup(PWM2_RPWM, 3921, 8);
+  }
+
+  //keep pulled high and drag low to activate, noise free safe
+  pinMode(WORKSW_PIN, INPUT_PULLUP);
+  pinMode(STEERSW_PIN, INPUT_PULLUP);
+  pinMode(REMOTE_PIN, INPUT_PULLUP);
+  pinMode(DIR1_RL_ENABLE, OUTPUT);
+
+  // Disable digital inputs for analog input pins
+  //pinMode(CURRENT_SENSOR_PIN, INPUT_DISABLE);
+  //pinMode(PRESSURE_SENSOR_PIN, INPUT_DISABLE);
+
+  //set up communication
+  Wire.begin();
+
+
+  EEPROM.get(0, EEread);  // read identifier
+
+  if (EEread != EEP_Ident)  // check on first start and write EEPROM
+  {
+    EEPROM.put(0, EEP_Ident);
+    EEPROM.put(10, steerSettings);
+    EEPROM.put(40, steerConfig);
+  } else {
+    EEPROM.get(10, steerSettings);  // read the Settings
+    EEPROM.get(40, steerConfig);
+  }
+
+  steerSettingsInit();
+
+  if (Autosteer_running) {
+    Serial.println("Autosteer running, waiting for AgOpenGPS");
+    // Autosteer Led goes Red if ADS1115 is found
+    //digitalWrite(AUTOSTEER_ACTIVE_LED, 0);
+    //digitalWrite(AUTOSTEER_STANDBY_LED, 1);
+  } else {
+    Autosteer_running = false;  //Turn off auto steer if no ethernet (Maybe running T4.0)
+                                //    if(!Ethernet_running)Serial.println("Ethernet not available");
+    Serial.println("Autosteer disabled, GPS only mode");
+    return;
+  }
+
+  // Initialize BNO080 lib
+  if (myIMU.begin())  //??? Passing NULL to non pointer argument, remove maybe ???
+  {
+    //Increase I2C data rate to 400kHz
+    Wire.setClock(400000);
+
+    delay(300);
+    // Use gameRotationVector and set REPORT_INTERVAL
+    myIMU.enableGameRotationVector(REPORT_INTERVAL);
+    useBNO08x = true;
+  } else {
+    Serial.println("BNO080 not detected at given I2C address.");
+  }
+
+}  // End of Setup
+
+void setup() {
+  // Setup Serial Monitor
+  Serial.begin(115200);
+
+  // Create WiFiManager object
+  WiFiManager wfm;
+  // Supress Debug information
+  wfm.setDebugOutput(false);
+
+  if (!wfm.autoConnect("ESP32TEST_AP")) {
+    // Did not connect, print error message
+    Serial.println("failed to connect and hit timeout");
+
+    // Reset and try again
+    ESP.restart();
+    delay(1000);
+  }
+
+  myip = WiFi.localIP();
+  // Connected!
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (udp.listen(8888)) {
+    Serial.print("UDP Listening on IP: ");
+    Serial.println(WiFi.localIP());
+    udp.onPacket([](AsyncUDPPacket packet) {
+      autoSteerPacketPerser(packet);
+    });
+  }
+  autosteerSetup();
+}
+
+void loop() {
+  ts.execute();
+}
---
+++ ESP32/Autosteer/zPackets.ino
@@ -0,0 +1,247 @@
+uint8_t* autoSteerUdpData;
+//Heart beat hello AgIO
+uint8_t helloFromIMU[] = { 128, 129, 121, 121, 5, 0, 0, 0, 0, 0, 71 };
+uint8_t helloFromAutoSteer[] = { 0x80, 0x81, 126, 126, 5, 0, 0, 0, 0, 0, 71 };
+int16_t helloSteerPosition = 0;
+
+//fromAutoSteerData FD 253 - ActualSteerAngle*100 -5,6, SwitchByte-7, pwmDisplay-8
+uint8_t PGN_253[] = { 0x80, 0x81, 126, 0xFD, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0xCC };
+int8_t PGN_253_Size = sizeof(PGN_253) - 1;
+
+//fromAutoSteerData FD 250 - sensor values etc
+uint8_t PGN_250[] = { 0x80, 0x81, 126, 0xFA, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0xCC };
+int8_t PGN_250_Size = sizeof(PGN_250) - 1;
+
+unsigned int AOGNtripPort = 2233;                 // port NTRIP data from AOG comes in
+unsigned int AOGAutoSteerPort = 8888;             // port Autosteer data from AOG comes in
+unsigned int portDestination = 9999;              // Port of AOG that listens
+IPAddress ipDes = IPAddress(255, 255, 255, 255);  //AOG IP
+void steerConfigInit() {
+  if (steerConfig.CytronDriver) {
+    pinMode(PWM2_RPWM, OUTPUT);
+  }
+}
+
+void autoSteerPacketPerser(AsyncUDPPacket packet) {
+  if (packet.length() < 5)
+    return;
+  autoSteerUdpData = packet.data();
+  if (autoSteerUdpData[0] == 0x80 && autoSteerUdpData[1] == 0x81 && autoSteerUdpData[2] == 0x7F)  //Data
+  {
+    switch (autoSteerUdpData[3]) {
+      case 0xFE:
+        {
+          if (!Autosteer_running) {
+            break;
+          }
+
+          gpsSpeed = ((float)(autoSteerUdpData[5] | autoSteerUdpData[6] << 8)) * 0.1;
+          gpsSpeedUpdateTimer = 0;
+
+          prevGuidanceStatus = guidanceStatus;
+
+          guidanceStatus = autoSteerUdpData[7];
+          guidanceStatusChanged = (guidanceStatus != prevGuidanceStatus);
+
+          //Bit 8,9    set point steer angle * 100 is sent
+          steerAngleSetPoint = ((float)(autoSteerUdpData[8] | ((int8_t)autoSteerUdpData[9]) << 8)) * 0.01;  //high low bytes
+
+          //Serial.print("steerAngleSetPoint: ");
+          //Serial.println(steerAngleSetPoint);
+
+          //Serial.println(gpsSpeed);
+
+          if ((bitRead(guidanceStatus, 0) == 0) || (gpsSpeed < 0.1) || (steerSwitch == 1)) {
+            watchdogTimer = WATCHDOG_FORCE_VALUE;  //turn off steering motor
+          } else                                   //valid conditions to turn on autosteer
+          {
+            watchdogTimer = 0;  //reset watchdog
+          }
+
+          //Bit 10 Tram
+          tram = autoSteerUdpData[10];
+
+          //Bit 11
+          relay = autoSteerUdpData[11];
+
+          //Bit 12
+          relayHi = autoSteerUdpData[12];
+
+          //----------------------------------------------------------------------------
+          //Serial Send to agopenGPS
+
+          int16_t sa = (int16_t)(steerAngleActual * 100);
+
+          PGN_253[5] = (uint8_t)sa;
+          PGN_253[6] = sa >> 8;
+
+          // heading
+          PGN_253[7] = (uint8_t)9999;
+          PGN_253[8] = 9999 >> 8;
+
+          // roll
+          PGN_253[9] = (uint8_t)8888;
+          PGN_253[10] = 8888 >> 8;
+
+          PGN_253[11] = switchByte;
+          PGN_253[12] = (uint8_t)pwmDisplay;
+
+          //checksum
+          int16_t CK_A = 0;
+          for (uint8_t i = 2; i < PGN_253_Size; i++)
+            CK_A = (CK_A + PGN_253[i]);
+
+          PGN_253[PGN_253_Size] = CK_A;
+
+          sendUdp(PGN_253, sizeof(PGN_253));
+
+          //Steer Data 2 -------------------------------------------------
+          if (steerConfig.PressureSensor || steerConfig.CurrentSensor) {
+            if (aog2Count++ > 2) {
+              //Send fromAutosteer2
+              PGN_250[5] = (byte)sensorReading;
+
+              //add the checksum for AOG2
+              CK_A = 0;
+
+              for (uint8_t i = 2; i < PGN_250_Size; i++) {
+                CK_A = (CK_A + PGN_250[i]);
+              }
+
+              PGN_250[PGN_250_Size] = CK_A;
+
+              sendUdp(PGN_250, sizeof(PGN_250));
+              aog2Count = 0;
+            }
+          }
+          break;
+        }
+      //steer settings
+      case 252:
+        {  //0xFC
+          if (!Autosteer_running) {
+            return;
+          }
+          //PID values
+          steerSettings.Kp = ((float)autoSteerUdpData[5]);  // read Kp from AgOpenGPS
+
+          steerSettings.highPWM = autoSteerUdpData[6];  // read high pwm
+
+          steerSettings.lowPWM = (float)autoSteerUdpData[7];  // read lowPWM from AgOpenGPS
+
+          steerSettings.minPWM = autoSteerUdpData[8];  //read the minimum amount of PWM for instant on
+
+          float temp = (float)steerSettings.minPWM * 1.2;
+          steerSettings.lowPWM = (byte)temp;
+
+          steerSettings.steerSensorCounts = autoSteerUdpData[9];  //sent as setting displayed in AOG
+
+          steerSettings.wasOffset = (autoSteerUdpData[10]);  //read was zero offset Lo
+
+          steerSettings.wasOffset |= (autoSteerUdpData[11] << 8);  //read was zero offset Hi
+
+          steerSettings.AckermanFix = (float)autoSteerUdpData[12] * 0.01;
+
+          //crc
+          //autoSteerUdpData[13];
+
+          //store in EEPROM
+          EEPROM.put(10, steerSettings);
+
+          // Re-Init steer settings
+          steerSettingsInit();
+          break;
+        }
+      case 251:  //251 FB - SteerConfig
+        {
+          uint8_t sett = autoSteerUdpData[5];  //setting0
+
+          if (bitRead(sett, 0)) steerConfig.InvertWAS = 1;
+          else steerConfig.InvertWAS = 0;
+          if (bitRead(sett, 1)) steerConfig.IsRelayActiveHigh = 1;
+          else steerConfig.IsRelayActiveHigh = 0;
+          if (bitRead(sett, 2)) steerConfig.MotorDriveDirection = 1;
+          else steerConfig.MotorDriveDirection = 0;
+          if (bitRead(sett, 3)) steerConfig.SingleInputWAS = 1;
+          else steerConfig.SingleInputWAS = 0;
+          if (bitRead(sett, 4)) steerConfig.CytronDriver = 1;
+          else steerConfig.CytronDriver = 0;
+          if (bitRead(sett, 5)) steerConfig.SteerSwitch = 1;
+          else steerConfig.SteerSwitch = 0;
+          if (bitRead(sett, 6)) steerConfig.SteerButton = 1;
+          else steerConfig.SteerButton = 0;
+          if (bitRead(sett, 7)) steerConfig.ShaftEncoder = 1;
+          else steerConfig.ShaftEncoder = 0;
+
+          steerConfig.PulseCountMax = autoSteerUdpData[6];
+
+          //was speed
+          //autoSteerUdpData[7];
+
+          sett = autoSteerUdpData[8];  //setting1 - Danfoss valve etc
+
+          if (bitRead(sett, 0)) steerConfig.IsDanfoss = 1;
+          else steerConfig.IsDanfoss = 0;
+          if (bitRead(sett, 1)) steerConfig.PressureSensor = 1;
+          else steerConfig.PressureSensor = 0;
+          if (bitRead(sett, 2)) steerConfig.CurrentSensor = 1;
+          else steerConfig.CurrentSensor = 0;
+          if (bitRead(sett, 3)) steerConfig.IsUseY_Axis = 1;
+          else steerConfig.IsUseY_Axis = 0;
+
+          //crc
+          //autoSteerUdpData[13];
+
+          EEPROM.put(40, steerConfig);
+
+          // Re-Init
+          steerConfigInit();
+        }
+      case 200:
+        {  // Hello from AgIO
+
+          if (Autosteer_running) {
+            int16_t sa = (int16_t)(steerAngleActual * 100);
+
+            helloFromAutoSteer[5] = (uint8_t)sa;
+            helloFromAutoSteer[6] = sa >> 8;
+
+            helloFromAutoSteer[7] = (uint8_t)helloSteerPosition;
+            helloFromAutoSteer[8] = helloSteerPosition >> 8;
+            helloFromAutoSteer[9] = switchByte;
+
+            sendUdp(helloFromAutoSteer, sizeof(helloFromAutoSteer));
+          }
+          if (useBNO08x) {
+            sendUdp(helloFromIMU, sizeof(helloFromIMU));
+          }
+        }
+      case 202:
+        {
+          //make really sure this is the reply pgn
+          if (autoSteerUdpData[4] == 3 && autoSteerUdpData[5] == 202 && autoSteerUdpData[6] == 202) {
+            ipDes = packet.remoteIP();
+            //hello from AgIO
+            uint8_t scanReply[] = { 128, 129, 126, 203, 7,
+                                    myip[0], myip[1], myip[2], myip[3],
+                                    myip[0], myip[1], myip[2], 23 };
+
+            //checksum
+            int16_t CK_A = 0;
+            for (uint8_t i = 2; i < sizeof(scanReply) - 1; i++) {
+              CK_A = (CK_A + scanReply[i]);
+            }
+            scanReply[sizeof(scanReply) - 1] = CK_A;
+
+            sendUdp(scanReply, sizeof(scanReply));
+          }
+        }
+    }
+  }
+}
+
+void sendUdp(uint8_t* data, uint8_t datalen) {
+  AsyncUDPMessage m = AsyncUDPMessage(datalen);
+  m.write(data, datalen);
+  udp.sendTo(m, ipDes, 9999);
+}
